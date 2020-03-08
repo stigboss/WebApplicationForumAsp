3a15d244f91e679c99875fa3aa1b4cde238c1c41
blackkiv <troolgames@outlook.com>
2020-03-08 09:20:50 +0200

    update 08.03

--- WebApplicationForum/Scripts/ValidationScript.js
+++ WebApplicationForum/Scripts/ValidationScript.js
@@ -1,24 +1,25 @@
-﻿$("#sendUserForm").click(
-function(e){
+﻿function checkUser(e){
+    Console.log("hi");
     e.preventDefault(); 
     const Nickname = $("input[name='Nickname']").val();
     if (Nickname.length == 0) {
         alert("Enter Nickname");
         return;
     }
-    var regexp = /[а-яё]/i;
+    /*var regexp = /[а-яё]/i;
     if (regexp.test(Nickname)) {
         alert("Invalid Nickname");
         return;
-    }
-    const RealName = $("input[name='RealName']").val();
-    regexp = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
-    if (!regexp.test(RealName)) {
-        alert("Invalid RealName");
-        return;
-    }
+    }*/
+    //const RealName = $("input[name='RealName']").val();
+    //regexp = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
+    //if (!regexp.test(RealName)) {
+    //    alert("Invalid RealName");
+    //    return;
+    //}
     const Password = $("input[name='PassHash']").val();
-    const RePassword = $("inpit[name='RePassword']").val();
+    const RePassword = $("input[name='RePassword']").val();
+    Console.log("pass: "+Password+"repass: "+RePassword)
     if (Password != RePassword) {
         alert("Password didn't mutch");
         return;
